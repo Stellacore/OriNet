3292ceacf182b20f94bf614eda320ac77ee59343
Stellacore <dave@stellacore.com>
2024-06-30 11:41:21 -0600

    demo/demo_statistics.cpp - placeholder skeleton for monte-carlo estimation of statistical relationships

---
+++ demo/demo_statistics.cpp
@@ -0,0 +1,51 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Stellacore Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
+/*! \file
+\brief Demonstration relationship of hexad maxMag to sigma{Loc,Ang} values.
+*/
+
+
+// #include "OriNet/random.hpp"
+
+#include <Engabra>
+#include <Rigibra>
+
+#include <iostream>
+
+
+/*! \brief Monte-Carlo simulation of hexad 'maxMag' statistics.
+ *
+ * Program generates output of that should be useful for fitting a
+ * heuristic model to predict hexad 'maxMag' values associated with
+ * the random::perturbedTransform() generated transformations.
+ */
+int
+main
+	()
+{
+	std::cout << "Hi from " << __FILE__ << '\n';
+}
+
