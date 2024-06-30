bc1b07704ae908d5a865f69f14c7728c035ea56b
Stellacore <dave@stellacore.com>
2024-06-29 18:20:56 -0600

    include/OriNet/compare.hpp - similarResult() function for comparing rigibra::Transform operator results

---
+++ include/OriNet/compare.hpp
@@ -0,0 +1,167 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Stellacore Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
+#ifndef OriNet_compare_INCL_
+#define OriNet_compare_INCL_
+
+
+#include <Rigibra>
+
+#include <algorithm>
+#include <limits>
+
+
+/*! \file
+\brief Utilities for comparing transformation results
+
+*/
+
+
+namespace orinet
+{
+
+	/*! \brief Max mag difference in basis vectors transformed by each xfm[12]
+	 *
+	 * Specifically, each data argument is used to transform the endpoints
+	 * of six basis vectors (i.e. +/- e_{1,2,3}). Vector differences are
+	 * computed between corresponding entities, and the maximum magnitude
+	 * of the six difference vectors is returned.
+	 */
+	inline
+	double
+	maxMagResultDifference
+		( rigibra::Transform const & xfm1
+		, rigibra::Transform const & xfm2
+		)
+	{
+		using namespace engabra::g3;
+
+		// R*(x-t)*Rr
+		// R*x*Rr - R*t*Rr
+		// +/- R*ek*Rr - R*t*Rr
+		// (-R*t*Rr) +/- R*ek*Rr 
+
+		// transform attitude changes for each of the +/- each basis vector
+		Vector const into_e1_1{ xfm1.theAtt(e1) };
+		Vector const into_e1_2{ xfm2.theAtt(e1) };
+
+		Vector const into_e2_1{ xfm1.theAtt(e2) };
+		Vector const into_e2_2{ xfm2.theAtt(e2) };
+
+		Vector const into_e3_1{ xfm1.theAtt(e3) };
+		Vector const into_e3_2{ xfm2.theAtt(e3) };
+
+		// apply rotation to the translation components of transformations
+		/*
+		Vector const into_rtr_1{ -xfm1.theAtt(xfm1.theLoc) };
+		Vector const into_rtr_2{ -xfm2.theAtt(xfm2.theLoc) };
+		Vector const into_rtr_delta{ into_rtr_2 - into_rtr_1 };
+		*/
+		Vector const into_rtr_1{ xfm1.theAtt(xfm1.theLoc) };
+		Vector const into_rtr_2{ xfm2.theAtt(xfm2.theLoc) };
+		Vector const into_rtr_delta{ into_rtr_1 - into_rtr_2 };
+
+		// compute corresponding vector differences
+		/*
+		std::array<Vector, 6u> const 
+			{ (into_rtr_2 + into_e1_2) - (into_rtr_1 + into_e1_1)
+			, (into_rtr_2 - into_e1_2) - (into_rtr_1 - into_e1_1)
+			, (into_rtr_2 + into_e2_2) - (into_rtr_1 + into_e2_1)
+			, (into_rtr_2 - into_e2_2) - (into_rtr_1 - into_e2_1)
+			, (into_rtr_2 + into_e3_2) - (into_rtr_1 + into_e3_1)
+			, (into_rtr_2 - into_e3_2) - (into_rtr_1 - into_e3_1)
+			};
+
+		std::array<Vector, 6u> const 
+			{ (into_rtr_2 - into_rtr_1) + into_e1_2 - into_e1_1
+			, (into_rtr_2 - into_rtr_1) - into_e1_2 + into_e1_1
+			, (into_rtr_2 - into_rtr_1) + into_e2_2 - into_e2_1
+			, (into_rtr_2 - into_rtr_1) - into_e2_2 + into_e2_1
+			, (into_rtr_2 - into_rtr_1) + into_e3_2 - into_e3_1
+			, (into_rtr_2 - into_rtr_1) - into_e3_2 + into_e3_1
+			};
+		std::array<Vector, 6u> const diffs
+			{ into_rtr_delta + into_e1_2 - into_e1_1
+			, into_rtr_delta - into_e1_2 + into_e1_1
+			, into_rtr_delta + into_e2_2 - into_e2_1
+			, into_rtr_delta - into_e2_2 + into_e2_1
+			, into_rtr_delta + into_e3_2 - into_e3_1
+			, into_rtr_delta - into_e3_2 + into_e3_1
+			};
+		*/
+		Vector const into_e1_delta{ into_e1_2 - into_e1_1 };
+		Vector const into_e2_delta{ into_e2_2 - into_e2_1 };
+		Vector const into_e3_delta{ into_e3_2 - into_e3_1 };
+
+		std::array<Vector, 6u> const diffs
+			{ into_rtr_delta + into_e1_delta
+			, into_rtr_delta - into_e1_delta
+			, into_rtr_delta + into_e2_delta
+			, into_rtr_delta - into_e2_delta
+			, into_rtr_delta + into_e3_delta
+			, into_rtr_delta - into_e3_delta
+			};
+
+		// compute max delta
+		double maxMag{ -1. };
+		for (Vector const & diff : diffs)
+		{
+			maxMag = std::max(maxMag, magnitude(diff));
+		}
+
+		return maxMag;
+	}
+		
+
+	/*! \brief True if both transforms produce similar output.
+	 *
+	 * Specifically, maxMagResultDifference() is used to compute the
+	 * the maximum magnitude difference that occurs when six basis
+	 * vectors are transformed with each argument. This maxmag
+	 * difference value is used to determine similarity of
+	 * operational results. Specifically, the two transforms are
+	 * considered to produce similar results if the computed
+	 * maximum magnitude is less than the provided tolerance value.
+	 *
+	 * Example:
+	 * \snippet test_nearness.cpp DoxyExample01
+	 */
+	inline
+	bool
+	similarResult
+		( rigibra::Transform const & xfm1
+		, rigibra::Transform const & xfm2
+		, double const & tol = std::numeric_limits<double>::epsilon()
+		)
+	{
+		// compute max delta
+		double const maxMag{ maxMagResultDifference(xfm1, xfm2) };
+		return (maxMag < tol);
+	}
+
+} // [orinet]
+
+
+#endif // OriNet_compare_INCL_
--- test/test_nearness.cpp
+++ test/test_nearness.cpp
@@ -28,33 +28,177 @@
 */
 
 
+#include "OriNet/compare.hpp"
+
 #include <Rigibra>
 
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
+#include <random>
 #include <sstream>
+#include <vector>
 
 
 namespace
 {
-	//! Examples for documentation
+	//! A transformation with arbitrary parameters values
+	inline
+	rigibra::Transform
+	aRandomTransform
+		()
+	{
+		// Range of translations - plus/minus this limit
+		constexpr double limLoc{ 10. };
+		// Range of rotation angles - plus/minus this limit
+		constexpr double limAng{ engabra::g3::pi };
+
+		// Configure pseudo-random number distribution generator
+		static std::mt19937 gen(7484020u);
+		static std::uniform_real_distribution<> distLocs(-limLoc, limLoc);
+		static std::uniform_real_distribution<> distAngs(-limAng, limAng);
+
+		// Use pseudo-random number generation to create transformation
+		using namespace engabra::g3;
+		return rigibra::Transform
+			{ Vector{ distLocs(gen), distLocs(gen), distLocs(gen) }
+			, rigibra::Attitude
+				{ rigibra::PhysAngle
+					{ distLocs(gen), distLocs(gen), distLocs(gen) }
+				}
+			};
+	}
+
+	//! Check computation of transform result differences
 	void
 	test0
 		( std::ostream & oss
 		)
 	{
+		// [DoxyExample00]
+
+		// check maxMagResultDifference for many pseudo-random transforms
+		constexpr std::size_t numXforms{ 32u*1024u };
+		for (std::size_t numXform{0u} ; numXform < numXforms ; ++numXform)
+		{
+			// Generate two transforms (the x[12]w0) to compare
+			rigibra::Transform const x1w0{ aRandomTransform() };
+			rigibra::Transform const x2w1{ aRandomTransform() };
+			rigibra::Transform const x2w0{ x2w1 * x1w0 };
+
+			// get max mag result error between transforms
+			using namespace engabra::g3;
+			// Note the error associated with zero should be less than
+			// the error associated with the +/- e_k values and therefore
+			// not affect the test case. (error at the transformed zero
+			// should be an interpolation of the error at the extrema)
+			std::vector<Vector> const locFroms
+				{ -e1, -e2, -e3, zero<Vector>(), e1, e2, e3 };
+			double expMaxMag{ -1 };
+			// evaluate the difference with explicit application of
+			// transformation operator()
+			for (Vector const & locFrom : locFroms)
+			{
+				Vector const into1{ x1w0(locFrom) };
+				Vector const into2{ x2w0(locFrom) };
+				Vector const diff{ into2 - into1 };
+				expMaxMag = std::max(expMaxMag, magnitude(diff));
+			}
+
+			// get computed values (which use abreviated formulae)
+			double const gotMaxMag
+				{ orinet::maxMagResultDifference(x1w0, x2w0) };
+
+			// check if the two methods of computation agree
+			// allow for computation noise (quadratic operations in attitude)
+			constexpr double tol
+				{ 128. * std::numeric_limits<double>::epsilon() };
+			if (! nearlyEquals(gotMaxMag, expMaxMag, tol))
+			{
+				double const difMaxMag{ gotMaxMag - expMaxMag };
+				oss << "Failure of maxMagResultDifference() test\n";
+				oss << "exp: " << expMaxMag << '\n';
+				oss << "got: " << gotMaxMag << '\n';
+				oss << "dif: " << engabra::g3::io::enote(difMaxMag) << '\n';
+				oss << "tol: " << engabra::g3::io::enote(tol) << '\n';
+				break;
+			}
+		}
+
+		// [DoxyExample00]
+	}
+
+	//! Examples for documentation
+	void
+	test1
+		( std::ostream & oss
+		)
+	{
 		// [DoxyExample01]
 
-		// create a rigid transform
-		rigibra::Transform const rigid
+		// create a first arbitrary rigid transform
+		rigibra::Transform const xform1
 			{ engabra::g3::Vector{ 1.1, 1.2, 1.3 }
 			, rigibra::Attitude(rigibra::PhysAngle{ 2.1, 2.2, 2.3 })
 			};
 
-		// check if it is equal to self
-		if (! nearlyEquals(rigid, rigid))
+		// create a second arbitrary rigid transform - distinctly different
+		rigibra::Transform const xform2
+			{ engabra::g3::Vector{ 1.6, 1.5, 1.4 }
+			, rigibra::Attitude(rigibra::PhysAngle{ 2.6, 2.5, 2.4 })
+			};
+
+		// transform collection of vectors using each of the two transforms
+		using namespace engabra::g3;
+		std::vector<Vector> const locFroms
+			{ -e1, -e2, -e3, zero<Vector>(), e1, e2, e3 };
+		// compute maximum magnitude vector difference of transformed vectors
+		// maxMag is a measure of maximum difference between transform results
+		double maxMag{ -1 };
+		for (Vector const & locFrom : locFroms)
+		{
+			Vector const into1{ xform1(locFrom) };
+			Vector const into2{ xform2(locFrom) };
+			Vector const diff{ into2 - into1 };
+			maxMag = std::max(maxMag, magnitude(diff));
+		}
+
+		// compute the nearness of the transformations...
+
+		// ... for case where tolerance is less than actual maxMax error
+		double const tolA{ maxMag - 1./1024. };
+		constexpr bool expNearA{ false };
+
+		bool const gotNearA{ orinet::similarResult(xform1, xform2, tolA) };
+		if (! (expNearA == gotNearA))
+		{
+			oss << "Failure of similarResult test 'A'\n";
+			oss << "exp: " << std::boolalpha << expNearA << '\n';
+			oss << "got: " << std::boolalpha << gotNearA << '\n';
+			oss << "tol: " << io::fixed(tolA) << '\n';
+			oss << "mag: " << io::fixed(maxMag) << '\n';
+		}
+
+		// ... for case where tolerance is more than actual maxMax error
+		double const tolB{ maxMag + 1./1024. };
+		constexpr bool expNearB{ true };
+		bool const gotNearB{ orinet::similarResult(xform1, xform2, tolB) };
+		if (! (expNearB == gotNearB))
+		{
+			oss << "Failure of similarResult test 'B'\n";
+			oss << "exp: " << std::boolalpha << expNearB << '\n';
+			oss << "got: " << std::boolalpha << gotNearB << '\n';
+			oss << "tol: " << io::fixed(tolB) << '\n';
+			oss << "mag: " << io::fixed(maxMag) << '\n';
+		}
+
+
+		// irrespective of similarity testing the two transforms
+		// are not "nearly" the same in the sense of numeric representations
+		if (  nearlyEquals(xform1, xform2))
 		{
 			oss << "Failure: of nearlyEquals() self test\n";
-			oss << "rigid: " << rigid << '\n';
+			oss << "xform1: " << xform1 << '\n';
 		}
 
 		// [DoxyExample01]
@@ -71,6 +215,7 @@ main
 	std::stringstream oss;
 
 	test0(oss);
+	test1(oss);
 
 	if (oss.str().empty()) // Only pass if no errors were encountered
 	{
